d94837c7924b07b11c5dab03808787ee6489d1b2
Brave Kim <ygflove@bible.ac.kr>
2021-10-27 03:41:39 +0900

    :bug: Fix QuizList css can't override issue

--- client/src/components/QuizList.jsx
+++ client/src/components/QuizList.jsx
@@ -3,53 +3,27 @@ import { Link } from "react-router-dom";
 import { ScrollMenu } from "react-horizontal-scrolling-menu";
 
 import { Grid, Paper } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
-import { yellow, blue } from "@mui/material/colors";
 
 import { AuthContext } from "../context/AuthContext";
-
-const useStyles = makeStyles((theme) => ({
-  root: {},
-  item: {
-    overflowX: "hidden",
-  },
-  grid: {
-    width: 100,
-    margin: "0px 20px 10px 0px",
-  },
-  child: {
-    padding: theme.spacing(3),
-  },
-  endPaper: {
-    "& .MuiPaper-root": {      
-    },
-    backgroundColor: yellow[300],
-    padding: theme.spacing(3),
-  },
-  proceedPaper: {
-    padding: theme.spacing(3),
-    backgroundColor: blue[300],
-  },
-}));
+import "./QuizList.css";
 
 // quizInfo memorize해서 넘기기
 const QuizList = ({ quizInfo }) => {
-  const classes = useStyles();
   const { record } = useContext(AuthContext);
   const chapters = quizInfo && quizInfo.group_by_chapter.buckets;
 
   const QuizItem = ({ chapterId }) => {
     return (
-      <Grid className={classes.grid}>
+      <Grid className="item-grid">
         <Paper
           className={
             record &&
             record[chapterId] &&
             record[chapterId].some((each) => each === null)
-              ? classes.proceedPaper
+              ? "item-paper-proceed"
               : record && Object.keys(record).includes(chapterId)
-              ? classes.endPaper
-              : classes.child
+              ? "item-paper-end"
+              : "item-paper"
           }
         >
           <Link to={`/quiz/${chapterId}`}>챕터{chapterId.split("_")[1]}</Link>
@@ -60,12 +34,12 @@ const QuizList = ({ quizInfo }) => {
   return (
     <>
       <h3>{quizInfo.key}</h3>
-      <ScrollMenu className={classes.root}>
+      <ScrollMenu>
         {chapters.map((chapter) => (
           <QuizItem
             key={chapter.key}
             chapterId={chapter.key}
-            className={classes.item}
+            className="item"
           />
         ))}
       </ScrollMenu>
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -2,32 +2,14 @@ import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 
 import { Paper, InputBase, IconButton } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
 import SearchIcon from "@mui/icons-material/Search";
 
 import { AuthContext } from "../context/AuthContext";
 import QuizList from "../components/QuizList";
 import logoImg from "../image/logo.gif";
+import "./MainPage.css";
 
-const useStyles = makeStyles((theme) => ({
-  headline: {
-    textAlign: "center",
-  },
-  logo: {
-    width: 140,
-    margin: "auto",
-    display: "block",
-  },
-  paper: {
-    display: "flex",
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-}));
 const MainPage = (_) => {
-  const classes = useStyles();
   const { setRecord } = useContext(AuthContext);
   const [subjects, setSubjects] = useState([]);
   const [searchKeyword, setSearchKeyword] = useState("");
@@ -55,10 +37,10 @@ const MainPage = (_) => {
   };
   return (
     <div>
-      <h2 className={classes.headline}>재학생들을 위한 기출문제 웹사이트</h2>
-      <img className={classes.logo} src={logoImg} alt="" />
-      <Paper component="form" className={classes.paper} onSubmit={onSubmit}>
-        <InputBase className={classes.input} onChange={onChange} />
+      <h2>재학생들을 위한 기출문제 웹사이트</h2>
+      <img src={logoImg} alt="" />
+      <Paper component="form" className="search-bar" onSubmit={onSubmit}>
+        <InputBase className="search-input" onChange={onChange} />
         <IconButton type="submit">
           <SearchIcon />
         </IconButton>
