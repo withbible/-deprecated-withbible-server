a796d1a6d6023d0e229959cd856de0c7d6f2d744
Brave Kim <ygflove@bible.ac.kr>
2023-02-02 23:23:43 +0900

    :white_check_mark: 한 챕터의 선택기록 Interaction 쿼리 변경

--- src/History/dao.js
+++ src/History/dao.js
@@ -44,10 +44,10 @@ exports.insertUserOption = async function (
   chapterSeq
 ) {
   let query = `
-    INSERT INTO quiz_user_option
-      (question_seq, question_option_seq, user_seq, chapter_seq)
-    VALUES
-  `;
+  INSERT INTO quiz_user_option
+    (question_seq, question_option_seq, user_seq, chapter_seq)
+  VALUES
+`;
 
   const values = Object.entries(bulk).map(
     ([key, value]) => `(${key}, ${value}, ${userSeq}, ${chapterSeq})`
@@ -65,24 +65,30 @@ exports.updateUserOption = async function (
   userSeq,
   chapterSeq
 ) {
-  let query = `
-    INSERT INTO quiz_user_option
-      (question_seq, question_option_seq, user_seq, chapter_seq)
-    VALUES
+  const prefixQuery = `
+    UPDATE quiz_user_option AS uo
+    JOIN(
   `;
 
   const values = Object.entries(bulk).map(
-    ([key, value]) => `(${key}, ${value}, ${userSeq}, ${chapterSeq})`
+    ([key, value]) => `
+      SELECT 
+        ${key} AS question_seq, 
+        ${value} AS new_question_option_seq      
+    `
   );
 
-  query += values.join();
-  query += `
-    ON DUPLICATE KEY UPDATE
-      question_seq = VALUES(question_seq),
-      question_option_seq = VALUES(question_option_seq),
-      user_seq = VALUES(user_seq),
-      chapter_seq = VALUES(chapter_seq);
+  const postfixQuery = `
+    ) AS vals 
+    ON uo.question_seq = vals.question_seq
+    SET question_option_seq = new_question_option_seq
+    WHERE uo.user_seq = ${userSeq}
+    AND uo.chapter_seq = ${chapterSeq};
   `;
+
+  let query = prefixQuery;
+  query += values.join("UNION ALL");
+  query += postfixQuery;
 
   await connection.query(query);
 };
