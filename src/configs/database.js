38181a0ab456a99da0ef0ac554aa8bd0dfdb1049
Yongki Kim <ygflove@likelion.org>
2023-05-17 16:01:40 +0900

    :bug: createPool작업 에러처리
    This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch().

--- src/configs/database.js
+++ src/configs/database.js
@@ -6,26 +6,41 @@ const { logger } = require("./logger");
 
 // CONSTANT
 const fileName = path.basename(__filename, ".js");
-
-// CONFIG
-const pool = mysql.createPool({
+const dbConfig = {
   host: process.env.SQL_HOST,
   user: process.env.SQL_USER,
   port: process.env.SQL_PORT,
   password: process.env.SQL_PASSWORD,
   database: process.env.SQL_DATABASE,
-});
+};
 
 // MAIN
-(async function () {
+// eslint-disable-next-line no-shadow
+function waitForDB(dbConfig, times = 1) {
   try {
-    await pool.query("SELECT 1");
+    const pool = mysql.createPool(dbConfig);
 
     logger.info("MariaDB 10.5 connected");
+    return pool;
   } catch (err) {
-    logger.error(`[${fileName}]_${err.message}`);
-    pool.end();
-  }
-})();
+    if (times > 5) {
+      logger.error(
+        `[${fileName}]_Unable to connect to database in ${times} attemps, exiting`
+      );
+      process.exit();
+    }
 
-module.exports = pool;
+    const backoff = 2 ** (times - 1) * 1000;
+    logger.warn(
+      `[${fileName}]_Unable to connect to database, trying again in ${backoff}ms`
+    );
+
+    setTimeout(backoff);
+    return waitForDB(dbConfig, times + 1);
+  }
+}
+
+module.exports = (function () {
+  const pool = waitForDB(dbConfig);
+  return pool;
+})();
